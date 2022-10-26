a2203535d87a84c05be8a6d63d5ee741718e9ca0
tyler <1291571584@qq.com>
2022-10-26 18:57:15 +0800

    1.0

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <iostream>
+
+using namespace  std;
+
+//添加动态库的lib
+#pragma comment(lib, "ws2_32.lib")
+
+int main(){
+
+    std::cout<<1;
+    return 0;
+}
