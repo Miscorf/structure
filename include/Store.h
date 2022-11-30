c4186b1d0af4ed48ddbf88b0c5074ae26379e7e4
tyler <1291571584@qq.com>
2022-11-30 17:17:35 +0800

    2.0thread

--- include/Store.h
+++ include/Store.h
@@ -4,7 +4,7 @@
 #include <winsock2.h>
 #include <vector>
 #include <iostream>
-#include <pthread.h>
+#include <atomic>
 #include "das_data.h"
 #include "HF5.h"
 
@@ -16,19 +16,19 @@ private:
     hsize_t dim[2]; // 存储的维度 dim[1]为数据大小
     // HF5 hf5;
     int store_num; // 存储系数
-    std::vector<char> v_buf;
+
     int buf_size = 0;
 
     char *read_buf;   //  读取数据缓冲区
-    char *handle_buf; // 处理数据缓冲区
+    char *handle_buf; //  处理数据缓冲区
 
-    int read_buf_size = 0;
+    int read_buf_size;
     int handle_buf_size = 0;
 
-    int read_buf_begin = 0;
-    int read_buf_end = 0;
-    int handle_buf_begin = 0;
-    int handle_buf_end = 0;
+    std::atomic<u_int> read_buf_begin;
+    std::atomic<u_int> read_buf_end;
+    std::atomic<u_int> handle_buf_begin;
+    std::atomic<u_int> handle_buf_end;
 
 public:
     Store(SOCKET id, int store_num, hsize_t _dim[]);
@@ -36,8 +36,9 @@ public:
 
     boolean recv_data(char *buf, int size);
     boolean recv_data_thread(char *buf, int size);
+    boolean write_hdf5_thread(char *file, char *dataset, char *wdata);
+
     boolean write_hdf5(char *file, char *dataset, char *wdata);
-    boolean write_hdf5(char *file, char *dataset, std::vector<das_data> wdata);
     // boolean do_store(int store_num, int sec);
     boolean do_store(char *file, char *dataset, int store_num, int sec);
     boolean do_store_thread(char *file, char *dataset, int store_num, int sec);
